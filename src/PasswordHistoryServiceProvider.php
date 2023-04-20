cd1e6720c8f13160037af733e7d9d514d85dffcf
Dilnei <dilnei.soethe@gmail.com>
2023-04-20 15:06:38 -0300

    Update PasswordHistoryServiceProvider.php
    we do not need this, because it is already on project

--- src/PasswordHistoryServiceProvider.php
+++ src/PasswordHistoryServiceProvider.php
@@ -43,7 +43,7 @@ class PasswordHistoryServiceProvider extends ServiceProvider
         $configFile = __DIR__.'/config/password_history.php';
 
         /*if($this->app->runningUnitTests()) {
-            $configFile = __DIR__.'/../tests/Requirements/config/password_history.php';
+            //$configFile = __DIR__.'/../tests/Requirements/config/password_history.php';
         }*/
 
         $this->mergeConfigFrom($configFile, 'password_history');
